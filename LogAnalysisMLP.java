9f9bd36e709fa43f137a99b6d25467370ed2b011
Tony <chunyu66@gmail.com>
2016-11-14 14:11:44 -0800

    Add files via upload

---
+++ LogAnalysisMLP.java
@@ -0,0 +1,166 @@
+package org.deeplearning4j.examples.feedforward;
+
+import org.apache.commons.io.IOUtils;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.examples.feedforward.classification.PlotUtil;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.weights.HistogramIterationListener;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.File;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+public class LogAnalysisMLP {
+    private static Logger log = LoggerFactory.getLogger(LogAnalysisMLP.class);
+
+    public static void main(String[] args) throws Exception {
+        int seed = 1234;
+        double learningRate = 0.00015;
+        int batchSize = 1;
+        int nEpochs = 1;
+
+        int numInputs = 5;
+        int numOutputs = 20;
+        int numHiddenNodes = 12;
+
+        //Load the training data:
+        RecordReader rr = new CSVRecordReader();
+        rr.initialize(new FileSplit(new File("src/main/resources/logAnalysis/taining.csv")));
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,5,numOutputs);
+//        rr.initialize(new FileSplit(new File("src/main/resources/logAnalysis/iris1.csv")));
+//        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,4,numOutputs);
+
+        //Load the test/evaluation data:
+        RecordReader rrTest = new CSVRecordReader();
+        rrTest.initialize(new FileSplit(new File("src/main/resources/logAnalysis/testing.csv")));
+        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,5,numOutputs);
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(seed)
+            //.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT) // use stochastic gradient descent as an optimization algorithm
+            .iterations(1)
+            .activation("tanh")
+            .weightInit(WeightInit.XAVIER)
+            .learningRate(learningRate)
+            .regularization(true).l2(1e-4).dropOut(0.00015)
+            //.updater(Updater.NESTEROVS).momentum(0.9)
+            .list()
+            .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
+                .build())
+            .layer(1, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes).weightInit(WeightInit.XAVIER).activation("relu")
+                .build())
+            .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .activation("softmax")
+                .nIn(numHiddenNodes).nOut(numOutputs).build())
+            .backprop(true).pretrain(false)
+            .build();
+
+
+
+        NormalizerMinMaxScaler preProcessor = new NormalizerMinMaxScaler();
+        preProcessor.fit(trainIter);
+        trainIter.setPreProcessor(preProcessor);
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(new HistogramIterationListener(1));
+        model.setListeners(new ScoreIterationListener(5));  //Print score every 10 parameter updates
+
+        log.info("Train model....");
+        for ( int n = 0; n < nEpochs; n++) {
+            model.fit( trainIter );
+//            while (trainIter.hasNext()) {
+//                DataSet m = trainIter.next();
+//                preProcessor.transform(m);
+//                model.fit(m);
+//            }
+
+        }
+
+        System.out.println("Evaluate model....");
+        Evaluation eval = new Evaluation(numOutputs);
+        //model.output(testIter);
+        while(testIter.hasNext()){
+            DataSet t = testIter.next();
+            preProcessor.transform(t);
+            INDArray features = t.getFeatureMatrix();
+            INDArray lables = t.getLabels();
+            INDArray predicted = model.output(features,false);
+
+            eval.eval(lables, predicted);
+        }
+
+        //Print the evaluation statistics
+        System.out.println(eval.stats());
+
+
+        //------------------------------------------------------------------------------------
+        //Training is complete. Code that follows is for plotting the data & predictions only
+
+/*        //Plot the data:
+        double xMin = 0;
+        double xMax = 1.0;
+        double yMin = -0.2;
+        double yMax = 0.8;
+
+        //Let's evaluate the predictions at every point in the x/y input space
+        int nPointsPerAxis = 100;
+        double[][] evalPoints = new double[nPointsPerAxis*nPointsPerAxis][2];
+        int count = 0;
+        for( int i=0; i<nPointsPerAxis; i++ ){
+            for( int j=0; j<nPointsPerAxis; j++ ){
+                double x = i * (xMax-xMin)/(nPointsPerAxis-1) + xMin;
+                double y = j * (yMax-yMin)/(nPointsPerAxis-1) + yMin;
+
+                evalPoints[count][0] = x;
+                evalPoints[count][1] = y;
+
+                count++;
+            }
+        }
+
+        INDArray allXYPoints = Nd4j.create(evalPoints);
+        INDArray predictionsAtXYPoints = model.output(allXYPoints);
+
+        //Get all of the training data in a single array, and plot it:
+        rr.initialize(new FileSplit(new File("src/main/resources/classification/linear_data_train.csv")));
+        rr.reset();
+        int nTrainPoints = 1000;
+        trainIter = new RecordReaderDataSetIterator(rr,nTrainPoints,0,2);
+        DataSet ds = trainIter.next();
+        PlotUtil.plotTrainingData(ds.getFeatures(), ds.getLabels(), allXYPoints, predictionsAtXYPoints, nPointsPerAxis);
+
+
+        //Get test data, run the test data through the network to generate predictions, and plot those predictions:
+        rrTest.initialize(new FileSplit(new File("src/main/resources/classification/linear_data_eval.csv")));
+        rrTest.reset();
+        int nTestPoints = 500;
+        testIter = new RecordReaderDataSetIterator(rrTest,nTestPoints,0,2);
+        ds = testIter.next();
+        INDArray testPredicted = model.output(ds.getFeatures());
+        PlotUtil.plotTestData(ds.getFeatures(), ds.getLabels(), testPredicted, allXYPoints, predictionsAtXYPoints, nPointsPerAxis);*/
+
+        System.out.println("****************Example finished********************");
+    }
+}
